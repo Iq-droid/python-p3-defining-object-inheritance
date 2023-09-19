b798196c02e49a3aabb047759a676d9adf28b2ac
Iqra <iqra.ali@student.moringaschool.com>
2023-09-19 11:42:07 +0300

    testing

--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,13 @@
 class Vehicle:
+    
+    def __init__(self, wheel_size, wheel_number):
+        self.wheel_size = wheel_size
+        self.wheel_nnumber = wheel_number
+        
+    def go(self):
+        return "vrrrrrroom"
+    
+    def fill_up_tank(self):
+        return "filling up!"
+        
     pass
